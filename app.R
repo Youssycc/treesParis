f80eb1acd12e81be480f293fcafb3cd462f34649
Youssef El Ouard <youselouard@gmail.com>
2021-08-29 18:05:04 +0200

    Added all files to project

---
+++ app.R
@@ -0,0 +1,78 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(leaflet)
+
+source("fetch-data.R")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("The Trees of Paris"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("circonference",
+                        "Circonference in centimeters",
+                        min = 0,
+                        max = 400,
+                        value = c(0,400)),
+            sliderInput("height",
+                        "Height in meters",
+                        min = 0,
+                        max = 30,
+                        value = c(0,30))
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           leafletOutput("treesmap")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    treesSurroundings <- fetch_trees_surroundings()
+    userTrees <- reactive({
+        treesSurroundings %>%
+            filter(between(CIRCONFERENCE..cm.,input$circonference[1], input$circonference[2])) %>%
+            filter(between(HAUTEUR..m., input$height[1], input$height[2]))
+    })
+    
+    
+    pal <- colorBin(
+        palette = "Blues",
+        domain = c(0,30),
+        bins = 6
+    )
+    
+    translateRadius <- function(circonf) {
+        circonf*10/400 + 1
+    }○
+    
+    output$treesmap <- renderLeaflet({
+        leaflet(userTrees()) %>% setView(lng = 2.2952842712402344,
+                                           lat = 48.875723794948826, zoom = 14.5) %>% 
+            addTiles() %>%
+            addCircleMarkers(fillOpacity = 0.9,
+                             color = ~pal(HAUTEUR..m.),
+                             radius = ~translateRadius(CIRCONFERENCE..cm.),
+                             stroke = FALSE
+            )
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
